6e447cc36711c42066f90a399ec950a2cec8b23e
DevX-Developer <91322153+DevX-Developer@users.noreply.github.com>
2021-09-24 18:22:47 +0700

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+name 'devx_ui-carhud'
+description 'Created By DevX'
+author 'DevX'
+discord 'https://discord.gg/bQV7AStZEV'
+version '0.0.1'
+
+client_script 'SimpleCarHUD_cl.lua'
